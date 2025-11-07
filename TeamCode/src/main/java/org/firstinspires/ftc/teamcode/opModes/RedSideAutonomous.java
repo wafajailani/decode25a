f7a921c5a6f67cf28b24098202bc0b4ef5ca6703
wafa jailani <wafajailani407@gmail.com>
2025-11-06 19:36:00 -0600

    edit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/RedSideAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/RedSideAutonomous.java
@@ -61,7 +61,7 @@ public class RedSideAutonomous extends CommandOpMode {
 
         TurnInPlace = follower.pathBuilder()
                 .addPath(
-                        new BezierLine(new Pose(90, 102, 112.000), new Pose(90, 112))  //new BezierLine(new Pose(144-44.750, 112.000), new Pose(144-45.000, 95.000))
+                        new BezierLine(new Pose(90, 102), new Pose(90, 102))  //new BezierLine(new Pose(144-44.750, 112.000), new Pose(144-45.000, 95.000))
                 )
                 .setLinearHeadingInterpolation(Math.toRadians(45), Math.toRadians(90))
                 .build();
